8b7bb4437b4cff7eb51ee000e1ef096111cb1edd
RianIslam <rianislamdr@gmail.com>
2021-03-25 00:53:09 +0600

    function in python

--- function_py.py
+++ function_py.py
@@ -1,6 +1,13 @@
 age = 38
+age2 = 28
 
 if age >18:
+    print('ooops i am adult')
+else:
+    print('oops not i am')
+
+
+if age2 >18:
     print('ooops i am adult')
 else:
     print('oops not i am')
