348d90470eb9b02cba69e0cb5d65f186c5533d1e
RianIslam <rianislamdr@gmail.com>
2021-03-25 00:57:58 +0600

    function

--- function_py.py
+++ function_py.py
@@ -1,13 +1,11 @@
-age = 38
-age2 = 28
 
-if age >18:
-    print('ooops i am adult')
-else:
-    print('oops not i am')
+def check_age(age):
+    
+    if age >18:
+        print('ooops i am adult')
+    else:
+        print('oops not i am')
 
 
-if age2 >18:
-    print('ooops i am adult')
-else:
-    print('oops not i am')
+check_age(18)
+check_age(22)
