40f0d6c7da385b92fe9f9a27cb17a877ed00483f
RianIslam <rianislamdr@gmail.com>
2021-03-25 00:14:14 +0600

    list in python

--- list_py.py
+++ list_py.py
@@ -1,7 +1,8 @@
-cars = ["bmw","tesla","mercedes","toyota"]
+cars = ["bmw","tesla","mercedes","toyota","ferrri"]
 print(len(cars))
 print(cars)
 print(cars[0])
 print(cars[1])
 print(cars[2])
 print(cars[3])
+print(cars[4])
